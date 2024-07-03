2cc30c8ba5b4845667db286b1505271ae02b7c63
Nara <naraestitch@gmail.com>
2024-07-03 22:49:24 +0200

    I don't know, I just added stuff. Goodluck

--- chrome/popup.js
+++ chrome/popup.js
@@ -34,3 +34,32 @@ document.addEventListener('DOMContentLoaded', function() {
   
     displayJobs();
   });
+
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("Snack-me").style.fontSize = "30px";
+  } else {
+    document.getElementById("Snack-me").style.fontSize = "90px";
+  }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("icon-bar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
